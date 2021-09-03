bb88cf11d045108d95db59ce1f1e7b795da55cb7
Naga Manikanta <nagamanikanta.satyanarayana@freshworks.com>
2021-09-03 08:46:07 +0530

    responsive layout design styles

--- src/components/InfoItem/styles.js
+++ src/components/InfoItem/styles.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import { red, green, grey } from "../../styles/colors";
+import { devices } from "../../utils/devices";
 
 export const ItemContainer = styled.div`
-  width: calc(25%);
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   color: ${(props) => (props.recovered ? green : props.confirmed ? red : grey)};
   h4 {
     opacity: 0.6;
+  }
+  @media ${devices.tablet} {
+    width: calc(25%);
   }
 `;
--- src/components/summary/styles.js
+++ src/components/summary/styles.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import { devices } from "../../utils/devices";
 
 export const MainContainer = styled.div`
   margin: 50px;
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
+
+  @media ${devices.tablet} {
+    flex-direction: row;
+  }
 `;
